0a19ce916a174b5d2008931fe963aa40df25c5b3
John Callahan <jcallahan@callahtech.com>
2023-09-18 22:14:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<div align="center">
+
+  # QR Code Site
+  https://johnnumber.one/
+
+  _Practical Joke: A mischievous trick played on someone, generally causing the victim to experience embarrassment, perplexity, confusion, or discomfort. A person who performs a practical joke is called a "prankster"._
+</div>
+
+Why? Do you really have nothing better to do?
+---------------------------------------------
+Some people might say so. However, I live a very exciting and adventure driven life so those people would obviously be incorrect.
+
+_Anyways, to the story..._
+
+Once upon a time, in an office where everyone is always very nice to each other, I was approached by someone asking if I knew of a QR code site that was free and could export svg files. Being the helpful person I am, I told them I would find one for them that would fufill all of their hopes and dreams.
+
+That night, I went home and purchased a domain name. I then created The Greatest QR Code Generator Ever Created:tm:. This QR code generator displays random insults (or other humurous messages) so every time it gets used, this very special person can bask in the glory of these carefully crafted messages and know that they are inferior.
+
+Want to add to the list?
+------------------------
+Create a pull request for [captions.json](https://github.com/johndc7/qr-code-site/blob/master/src/captions.json). Once your pull request is approved, it will automatically display on the site.
+- Make sure to format your JSON file correctly. You can use something like https://jsonlint.com/ if you want to make sure.
+- Your submission should make sense in the context of "Want a QR code? {insert joke here}"
+- It should be funny. I hope I don't have to explain this one.
+- This is mainly a joke targeted at someone at work. All submissions should be SFW (don't get me fired).
+
+Other Improvements / Bug Fixes
+------------------------------
+Create a PR, once merged, it will be deployed to the site.
---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect, createRef } from 'react';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
+
+import QRCode from 'react-qr-code';
+
+import captions from './captions.json';
+
+const theme = createTheme({ palette: {mode: 'dark'} })
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: {
+      xs: 2,
+      lg: '5%'
+    },
+    alignItems: 'center'
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: {
+      xs: '90%',
+      lg: '50%'
+    },
+    padding: 3,
+    gap: 1
+  },
+  qrcode: {
+    margin: 'auto',
+    maxWidth: '100%'
+  }
+}
+
+function saveSvg(svg){
+  // return console.log(encodeURI(svg))
+
+  let uri = 'data:attachment/svg;charset=utf-8,' + encodeURIComponent(svg);
+
+  let downloadLink = document.createElement("a");
+  downloadLink.href = uri;
+  downloadLink.download = "qr.svg";
+
+  document.body.appendChild(downloadLink);
+  console.log(downloadLink)
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+function App() {
+  const [value, setValue] = useState();
+  const [fgColor, setFgColor] = useState('#000');
+  const [bgColor, setBgColor] = useState('#fff');
+  const [size, setSize] = useState(275);
+  const [caption, setCaption] = useState();
+  const [captionClickCount, setCaptionClickCount] = useState(1);
+
+  const codeRef = createRef();
+
+  useEffect(() => {
+    setCaption(captions[Math.floor(Math.random() * captions.length)]);
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={styles.root}>
+        <Typography variant="h4">Need a QR code?</Typography>
+        <Link underline="none" color="inherit" href="#" onClick={(e) => {
+          e.preventDefault();
+          if(captionClickCount < 5){
+            setCaption(captions[Math.floor(Math.random() * captions.length)])
+            setCaptionClickCount(captionClickCount + 1)
+          } else
+            window.location = "https://github.com/johndc7/qr-code-site#readme"
+        }} variant="caption">{caption}</Link>
+        <Paper elevation={3} sx={styles.paper}>
+          <QRCode ref={codeRef} fgColor={fgColor} bgColor={bgColor} style={styles.qrcode} size={size} value={value || window.location.href}/>
+          <TextField value={fgColor} onChange={e => setFgColor('#' + e.target.value.replace('#',''))} label="Foreground Color" autoComplete="none"/>
+          <TextField value={bgColor} onChange={e => setBgColor('#' + e.target.value.replace('#',''))} label="Background Color" autoComplete="none"/>
+          <Box>
+            <Typography gutterBottom>Size</Typography>
+            <Slider valueLabelDisplay="auto" min={50} max={500} value={size} onChange={e => setSize(e.target.value)} label="Size"/>
+          </Box>
+          <TextField multiline rows={3} onChange={e => setValue(e.target.value)} label="Value" autoComplete="none"/>
+          <Button disabled={value == null || value == ''} variant="contained" onClick={() => saveSvg(codeRef.current.outerHTML)}>Save</Button>
+        </Paper>
+      </Box>
+    </ThemeProvider>
+  )
+}
+
+export default App
